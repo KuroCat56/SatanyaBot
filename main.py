a3c8d4bb2e67099bdbb73f859c9e6b61197bac88
Nelson N. Minero <nelsonnminero@yahoo.com>
2021-02-02 03:51:10 +0000

    Terminada la respuesta con la API de kaomoji

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import discord
+from discord.ext import commands
 import os
 import requests
 import json
 import random
+
 #keep_alive: formato para mantener al bot activo 24/7 mediante un ping generado cada 5 min
 from keep_alive import keep_alive
 
 client = discord.Client()
 
 #lista de palabras triggers en un mensaje común que hacen trigger al bot 
-hola = ["hola", "buenas", "wenas", "hi", "hello"]
+hola = ["hola", "buenas", "wenas", "hello"]
 readHola = open("hello_list.txt")
 
 #Zenquotes api para experimentar con las api
@@ -20,6 +22,12 @@ def get_quote():
   quote = "**" + json_data[0]['q'] + "**" + " -" + "*" + json_data[0]['a'] + "*"
   return(quote)
 
+def get_kao():
+  response = requests.get("http://kaomoji.n-at.me/random.json")
+  json_data = json.loads(response.text)
+  kaomoji = json_data['record']['text']
+  return(kaomoji)
+
 #on_ready: Cuando el bot esté activo y funcional mandará un mensaje confirmando que está corriendo.
 @client.event
 async def on_ready():
@@ -27,6 +35,13 @@ async def on_ready():
   #Genera el estado de "Jugando" con la descripción name=''
   await client.change_presence(activity=discord.Game(name='>nya'))
 
+#Uso del framework de comandos de discord.exit
+#Se especifica el prefijo para todos los demás comandos
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 @client.event
 #on_message: Cuando recibe un mensaje, actúa si el mensaje es de otro miembro y no del propio bot.
@@ -36,9 +51,12 @@ async def on_message(message):
   # variable msg para acortar 
   msg = message.content
 #.startwith: El trigger que lee el bot para reaccionar según el comando especificado
-  if msg.startswith('>quote'):
+  if msg.startswith('>quo'):
     quote = get_quote()
     await message.channel.send(quote)
+  if msg.startswith('>kao'):
+    kaomoji = get_kao()
+    await message.channel.send(kaomoji)
 #Sección de trigger para saludar
   if any (word in msg.lower() for word in hola):
     await message.channel.send(readHola.readline())
