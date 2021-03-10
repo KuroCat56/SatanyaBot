16daa1a046a40630641db729401e5c17dad004ff
Nelson N. Minero <nelson_minero@yahoo.com>
2021-03-10 10:56:45 -0600

    Modificada sección de quotes

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ satanyaDespide = readAdios
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
-    quote = "**" + json_data[0]['q'] + "**" + " -" + "*" + json_data[0]['a'] + "*"
+    quote = "**" + json_data[0]['q'] + "**" + " -" + json_data[0]['a']
     return (quote)
 
 
