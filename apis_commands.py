b8c173c4d69afc3178e78eace2807f5571884047
Nelson N. Minero <nelsonnminero@yahoo.com>
2021-03-19 16:57:30 +0000

    Creados comandos de meme y animeme usando la API de Reddit

--- apis_commands.py
+++ apis_commands.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
+import discord
 import requests
 import json
+import praw
+import private.reddit
+import random
 
 #API de Kaomojis
 def get_kao():
@@ -17,21 +21,57 @@ def get_quote():
     quote = "**" + json_data[0]['q'] + "**" + " -" + json_data[0]['a']
     return (quote)
 
+#API de Reddit
+reddit = praw.Reddit(client_id = private.reddit.client_id,
+                    client_secret = private.reddit.secret,
+                    username = private.reddit.username,
+                    password = private.reddit.password,
+                    user_agent = private.reddit.user_agent)
+#Construcción para comando nya>meme
+subreddit_memes = reddit.subreddit("memes")
+all_subs_memes = []
+top = subreddit_memes.top(limit = 50)
+for submission in top:
+  all_subs_memes.append(submission)
+  random_sub_memes = random.choice(all_subs_memes)
+  #Creación del embed
+  url_memes = random_sub_memes.url
+  em_memes = discord.Embed(title = None)
+  em_memes.set_image(url = url_memes)
+#Construcción para comando nya>animeme
+subreddit_animemes = reddit.subreddit("animemes")
+all_subs_animemes = []
+top = subreddit_animemes.top(limit = 50)
+for submission in top:
+  all_subs_animemes.append(submission)
+  random_sub_animemes = random.choice(all_subs_animemes)
+  #Creación del embed
+  url_animemes = random_sub_animemes.url
+  em_animemes = discord.Embed(title = None)
+  em_animemes.set_image(url = url_animemes)
+
 class apis_commands(commands.Cog):
   """Comandos que requieren de alguna API"""
 
   def __init__(self, client: commands.Bot):
     self.client = client
-
+#Comando que envía quotes
   @commands.command(name="quo")
   async def quo(self, ctx: commands.Context):
     quote = get_quote()
     await ctx.send(quote)
-
+#Comando que envía kaomojis
   @commands.command(name="kao")
   async def kao(self, ctx: commands.Context):
     kao = get_kao()
     await ctx.send(kao)
+#Comandos que envían memes
+  @commands.command(name="meme")
+  async def meme(self, ctx: commands.Context):
+    await ctx.send(embed = em_memes)
+  @commands.command(name="animeme")
+  async def animeme(self, ctx: commands.Context):
+    await ctx.send(embed = em_animemes)
 
 
 def setup(client: commands.Bot):
