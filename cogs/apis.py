0ff44a2c0252121684c0d90d9e9fa9c82e046757
Nelson N. Minero <nelson_minero@yahoo.com>
2021-05-08 22:49:37 -0600

    Inicio de mejora comando quo

--- cogs/apis.py
+++ cogs/apis.py
@@ -15,7 +15,7 @@ def get_kao():
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
-    quote = "**" + json_data[0]['q'] + "**" + " -" + json_data[0]['a']
+    quote = "**" + json_data[0]['q'] #+ "**" + " -" + json_data[0]['a']
     return (quote)
 
 class apis(commands.Cog):
