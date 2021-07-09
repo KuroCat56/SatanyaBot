bee08c8297151ebc079d039ae7c7d207675087af
Kuro <nelson_minero@yahoo.com>
2021-07-08 20:04:16 -0600

    Más correciones menores

--- cogs/general.py
+++ cogs/general.py
@@ -238,7 +238,7 @@ class general(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, 
 
         em = discord.Embed(description=created_at, color=0xfbf9fa)
         em.add_field(name='Owner', value=guild.owner, inline=False)
-        em.add_field(name='Miembros', value=len(guild.members))
+        em.add_field(name='Miembros', value=len(guild.members), inline=False)
         em.add_field(name='Roles', value=len(guild.roles))
         em.add_field(name='<:dscrd_channel:851449868722896936> Canales de texto', value=len(guild.text_channels))
         em.add_field(name='<:dscrd_voice:862873567204212776> Canales de voz', value=len(guild.voice_channels))
