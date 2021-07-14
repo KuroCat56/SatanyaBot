5d4888b4f6ccf47914c55a2c7507df84558fa620
Kuro <nelson_minero@yahoo.com>
2021-07-13 23:45:32 -0600

    Añadido comando swirl

--- cogs/img.py
+++ cogs/img.py
@@ -437,5 +437,22 @@ class img(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, comm
       embed.set_footer(text=f"Solicitado por {ctx.message.author} │ dagpi.xyz", icon_url=member.avatar_url)
       await ctx.reply(file=file_plrd, embed=embed, mention_author=False)
 
+  @commands.command()
+  async def swirl(self, ctx, member: discord.Member=None):
+    """
+    Vamos a deformar un poco
+    """
+    if member is None:
+      member = ctx.author
+    async with ctx.typing():
+      url_swrl = str(member.avatar_url_as(static_format="png", size=1024))
+      img_swrl = await dagpi.image_process(ImageFeatures.swirl(), url_swrl)
+      file_swrl = discord.File(fp=img_swrl.image,filename=f"swirl.{img_swrl.format}")
+
+      embed = discord.Embed(color=ctx.author.color)
+      embed.set_image(url=f"attachment://swirl.{img_swrl.format}")
+      embed.set_footer(text=f"Solicitado por {ctx.message.author} │ dagpi.xyz", icon_url=member.avatar_url)
+      await ctx.reply(file=file_swrl, embed=embed, mention_author=False)
+
 def setup(bot: commands.Bot):
     bot.add_cog(img(bot))
