678d1e0dde6f30822b8f21f6f23e7374453fdd48
Kuro <nelson_minero@yahoo.com>
2021-07-28 14:03:03 -0600

    Mejor ping

--- cogs/general.py
+++ cogs/general.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from psutil import Process
 from os import getpid
 import pkg_resources
+import time
 
 def lines_of_code():
     """
@@ -55,9 +56,31 @@ class general(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, 
         """
         ¡Pong!
         """
+        pings = []
+        number = 0
+        typings = time.monotonic()
+        await ctx.trigger_typing()
+        typinge = time.monotonic()
+        typingms = round((typinge - typings) * 1000)
+        pings.append(typingms)
+        latencyms = round(self.bot.latency * 1000)
+        pings.append(latencyms)
+        discords = time.monotonic()
+        url = "https://discordapp.com/"
+        async with self.bot.session.get(url) as resp:
+            if resp.status is 200:
+                discorde = time.monotonic()
+                discordms = round((discorde-discords)*1000)
+                pings.append(discordms)
+                discordms = f"{discordms}ms"
+            else:
+                discordms = "Failed"
+        for ms in pings:
+            number += ms
+        average = round(number / len(pings))
         embed = discord.Embed(
           title="🏓 Pong",
-            description=(f'📨 Envío de mensajes: **{round(self.bot.latency * 1000)}ms**'),
+            description=(f'<a:dscrd_typing:862837114240237599> Escribiendo: **{typingms}ms**\n <a:dscrd_loading:866731675547336721> Ping: **{latencyms}ms**\n <a:clyde:846625894395412480> Discord: **{discordms}ms**'),
             color=0xfbf9fa
             #timestamp=datetime.utcnow()
         )
