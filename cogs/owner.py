a654e2f37676401d6c03152871fe047d2ceaf6f7
Kuro <nelson_minero@yahoo.com>
2021-08-01 17:47:57 -0600

    Corrección append

--- cogs/owner.py
+++ cogs/owner.py
@@ -102,9 +102,10 @@ class OwnerCog(commands.Cog, command_attrs=dict(hidden=True)):
                 try:
                     self.bot.unload_extension(f"cogs.{filename[:-3]}")
                     self.bot.load_extension(f"cogs.{filename[:-3]}")
+                    
+                    cogs.append(f"{filename}")
                 except Exception as error:
                     await ctx.reply(f"Nope: {filename}: {error}", mention_author=False)
-                cogs = [" "].join(f"{filename[:-3]}")
         await ctx.reply(f"Recargué los siguientes cogs: {cogs}", mention_author=False)
 
     @commands.command()
