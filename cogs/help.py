0bf7ec54d3ea65684ac62813888c071b4b36cf34
Kuro <nelson_minero@yahoo.com>
2021-08-01 18:20:15 -0600

    Tercer arreglo help

--- cogs/help.py
+++ cogs/help.py
@@ -47,7 +47,8 @@ class HelpCommand(commands.HelpCommand):
           usable += amount_commands
           value = "\t".join(f"`{i.name}`" for i in commands)
           embed.add_field(name = cog.qualified_name, value = value, inline=False)
-      embed.add_field(text="test", value=f"{len(self.bot.commands)} commands | {usable} usable")
+          
+      embed.add_field(name="test", value=f"{len(self.bot.commands)} commands | {usable} usable")
       embed.set_footer(text=self.footer())
       await self.get_destination().send(embed=embed)
 
