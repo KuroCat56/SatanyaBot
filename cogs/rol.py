191d2199257259d8ca4c1b1623b35e670b82e0f0
Kuro <nelson_minero@yahoo.com>
2021-08-11 16:51:59 -0600

    Contrucción de comando slap

--- cogs/rol.py
+++ cogs/rol.py
@@ -12,6 +12,7 @@ BITE = "https://purrbot.site/api/img/sfw/bite/gif"
 DANCE = "https://purrbot.site/api/img/sfw/dance/gif"
 PAT = "https://purrbot.site/api/img/sfw/pat/gif"
 CUDDLE = "https://purrbot.site/api/img/sfw/cuddle/gif"
+SLAP = "https://purrbot.site/api/img/sfw/slap/gif"
 
 #Si existe algún problema con al api
 ERROR = "Parece que hay un problema con la API o con mi procesamiento. Usa `nya>help` para más información o acude a mi server de soporte usando `nya>invite`"
@@ -47,9 +48,16 @@ def get_pat():
 def get_cuddle():
     response = requests.get(f"{CUDDLE}")
     json_data = json.loads(response.text)
-    cuddle= json_data['link']
+    cuddle = json_data['link']
     error_cuddle = json_data['error']
     return cuddle, error_cuddle
+
+def get_slap():
+    response = requests.get(f"{SLAP}")
+    json_data = json.loads(response.text)
+    slap = json_data['link']
+    error_slap = json_data['error']
+    return slap, error_slap
 
 class rol(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 5, commands.BucketType.user)}):
   
@@ -162,6 +170,25 @@ class rol(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 5, comma
       else:
         await ctx.reply(f"{ERROR}")
 
+  @commands.command(name="slap")
+  async def slap(self, ctx, member: discord.Member=None):
+    """
+    Porque a veces alguien necesita una cachetada (con cariño)
+    """
+    slap, error = get_slap()
+    if member is None:
+      message = "¡No puedes pegarte a tí mismo!\nA no ser que te guste..."
+      await ctx.reply(message, mention_author=False)
+    else:
+      if error is not "true":
+        async with ctx.typing():
+          embed = discord.Embed(
+          description = f"💢 **{ctx.author.name}** ha cachetado a **{member.name}**", color=discord.Colour.random())
+          embed.set_image(url = f"{pat}")
+          embed.set_footer(text=f"{PURR_FOOTER}", icon_url=f"{PURR}")
+          await ctx.send(embed = embed)
+      else:
+        await ctx.reply(f"{ERROR}")
 
 def setup(bot: commands.Bot):
     bot.add_cog(rol(bot))
