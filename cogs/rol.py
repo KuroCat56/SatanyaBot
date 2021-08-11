a28162f832969c7b8af71129436b67f4fc8749b5
Kuro <nelson_minero@yahoo.com>
2021-08-11 16:12:18 -0600

    Contrucción completa de comando pat

--- cogs/rol.py
+++ cogs/rol.py
@@ -10,6 +10,7 @@ PURR_FOOTER = "Powered by PurrBotAPI"
 HUG = "https://purrbot.site/api/img/sfw/hug/gif"
 BITE = "https://purrbot.site/api/img/sfw/bite/gif"
 DANCE = "https://purrbot.site/api/img/sfw/dance/gif"
+PAT = "https://purrbot.site/api/img/sfw/pat/gif"
 
 #Si existe algún problema con al api
 ERROR = "Parece que hay un problema con la API o con mi procesamiento. Usa `nya>help` para más información o acude a mi server de soporte usando `nya>invite`"
@@ -34,6 +35,13 @@ def get_dance():
     dance = json_data['link']
     error_dance = json_data['error']
     return dance, error_dance
+
+def get_pat():
+    response = requests.get(f"{PAT}")
+    json_data = json.loads(response.text)
+    pat = json_data['link']
+    error_pat = json_data['error']
+    return pat, error_pat
 
 class rol(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 5, commands.BucketType.user)}):
   
@@ -105,6 +113,26 @@ class rol(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 5, comma
             await ctx.send(embed = embed)
     else:
       await ctx.reply(f"{ERROR}")
+
+  @commands.command(name="pat", aliases=["headpat"])
+  async def pat(self, ctx, member: discord.Member=None):
+    """
+    ¿Alguien se merece unos pat-pat?
+    """
+    pat, error = get_pat()
+    if member is None:
+      message = "¡No puedes darte pat-pats a tí mismo!\nPero puede darme unos a mí (o´▽`o)"
+      await ctx.reply(message, mention_author=False)
+    else:
+      if error is not "true":
+        async with ctx.typing():
+          embed = discord.Embed(
+          description = f"❣️ **{ctx.author.name}** le ha dado unos pat-pats a **{member.name}**", color=discord.Colour.random())
+          embed.set_image(url = f"{pat}")
+          embed.set_footer(text=f"{PURR_FOOTER}", icon_url=f"{PURR}")
+          await ctx.send(embed = embed)
+      else:
+        await ctx.reply(f"{ERROR}")
 
 def setup(bot: commands.Bot):
     bot.add_cog(rol(bot))
