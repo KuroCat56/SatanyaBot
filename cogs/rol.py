7029123f6deb1d4612e08e52e1674c024d130405
Kuro <nelson_minero@yahoo.com>
2021-08-11 15:29:22 -0600

    Adición de BITE y ERROR

--- cogs/rol.py
+++ cogs/rol.py
@@ -7,6 +7,10 @@ import json
 PURR = "https://docs.purrbot.site/assets/img/logo.png" #pfp purrbot
 
 HUG = "https://purrbot.site/api/img/sfw/hug/gif"
+BITE = "https://purrbot.site/api/img/sfw/bite/gif"
+
+#Si existe algún problema con al api
+ERROR = "Parece que hay un problema con la API o con mi procesamiento. Usa `nya>help` para más información o acude a mi server de soporte usando `nya>invite`"
 
 def get_hug():
     response = requests.get(f"{HUG}")
@@ -44,7 +48,7 @@ class rol(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 5, comma
           embed.set_footer(text=f"Powered by PurrBotAPI", icon_url=f"{PURR}")
           await ctx.send(embed = embed)
       else:
-        await ctx.reply("Parece que hay un problema con la API o con mi procesamiento. Usa `nya>help` para más información o acude a mi server de soporte usando `nya>invite`")
+        await ctx.reply(f"{ERROR}")
 
 
 def setup(bot: commands.Bot):
