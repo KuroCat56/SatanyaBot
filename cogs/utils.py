8f8a7a5e283c92f94fbe3a3e155664d16822798d
Kuro <nelson_minero@yahoo.com>
2021-09-24 18:39:12 -0600

    Arregladas variables agregadas a translate

--- cogs/utils.py
+++ cogs/utils.py
@@ -272,7 +272,7 @@ class utils(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 5, com
       await ctx.send("Parece que hubo un error.")
 
   @commands.command(aliases = ["traducir", "traductor"])
-  async def translate(self, ctx, *, lang, texto):
+  async def translate(self, ctx, lang : str, texto):
     """
     Traducción rápida en Google.
 
