b8c398ceea3b1ac5b3cab2003c672c4d40cc49d5
Kuro <nelson_minero@yahoo.com>
2021-09-27 22:36:22 -0600

    Añadido comando like

--- cogs/img.py
+++ cogs/img.py
@@ -575,6 +575,36 @@ class img(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, comm
       embed.set_footer(text=f"Solicitado por {ctx.message.author} │ api.devs-hub.xyz", icon_url=member.avatar_url)
       await ctx.reply(embed=embed, mention_author=False)
 
+  @commands.command()
+  async def simp(self, ctx, member: discord.Member=None):
+    """
+    Certified simp moment
+    """
+    if member is None:
+      member = ctx.author
+    async with ctx.typing():
+      url_smp = str(member.avatar_url_as(static_format="png", size=1024))
+
+      embed = discord.Embed(color=ctx.author.color)
+      embed.set_image(url=f"https://api.devs-hub.xyz/simp?image={url_smp}")
+      embed.set_footer(text=f"Solicitado por {ctx.message.author} │ api.devs-hub.xyz", icon_url=member.avatar_url)
+      await ctx.reply(embed=embed, mention_author=False)
+
+  @commands.command()
+  async def like(self, ctx, member: discord.Member=None):
+    """
+    [Everyone liked that]
+    """
+    if member is None:
+      member = ctx.author
+    async with ctx.typing():
+      url_lk = str(member.avatar_url_as(static_format="png", size=1024))
+
+      embed = discord.Embed(color=ctx.author.color)
+      embed.set_image(url=f"https://api.devs-hub.xyz/like?image={url_lk}")
+      embed.set_footer(text=f"Solicitado por {ctx.message.author} │ api.devs-hub.xyz", icon_url=member.avatar_url)
+      await ctx.reply(embed=embed, mention_author=False)
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(img(bot))
