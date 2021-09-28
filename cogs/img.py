6546fd8167e6ded2ee48a7d23703215ff3998535
Kuro <nelson_minero@yahoo.com>
2021-09-27 22:42:32 -0600

    Añadido comando gun

--- cogs/img.py
+++ cogs/img.py
@@ -635,5 +635,20 @@ class img(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, comm
       embed.set_footer(text=f"Solicitado por {ctx.message.author} │ api.devs-hub.xyz", icon_url=member.avatar_url)
       await ctx.reply(embed=embed, mention_author=False)
 
+  @commands.command()
+  async def gun(self, ctx, member: discord.Member=None):
+    """
+    Plomo o plata
+    """
+    if member is None:
+      member = ctx.author
+    async with ctx.typing():
+      url_gn = str(member.avatar_url_as(static_format="png", size=1024))
+
+      embed = discord.Embed(color=ctx.author.color)
+      embed.set_image(url=f"https://api.devs-hub.xyz/gun?image={url_gn}")
+      embed.set_footer(text=f"Solicitado por {ctx.message.author} │ api.devs-hub.xyz", icon_url=member.avatar_url)
+      await ctx.reply(embed=embed, mention_author=False)
+
 def setup(bot: commands.Bot):
     bot.add_cog(img(bot))
