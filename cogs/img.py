015f324a8f87e70c27ccc7b92f1c2f7cd6a59790
Kuro <nelson_minero@yahoo.com>
2021-09-27 22:43:47 -0600

    Añadido comando grab

--- cogs/img.py
+++ cogs/img.py
@@ -650,5 +650,20 @@ class img(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, comm
       embed.set_footer(text=f"Solicitado por {ctx.message.author} │ api.devs-hub.xyz", icon_url=member.avatar_url)
       await ctx.reply(embed=embed, mention_author=False)
 
+  @commands.command()
+  async def grab(self, ctx, member: discord.Member=None):
+    """
+    ¡Ven aquí!
+    """
+    if member is None:
+      member = ctx.author
+    async with ctx.typing():
+      url_grb = str(member.avatar_url_as(static_format="png", size=1024))
+
+      embed = discord.Embed(color=ctx.author.color)
+      embed.set_image(url=f"https://api.devs-hub.xyz/grab?image={url_grb}")
+      embed.set_footer(text=f"Solicitado por {ctx.message.author} │ api.devs-hub.xyz", icon_url=member.avatar_url)
+      await ctx.reply(embed=embed, mention_author=False)
+
 def setup(bot: commands.Bot):
     bot.add_cog(img(bot))
