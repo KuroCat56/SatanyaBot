081375d106111a9ef45096d33792ffe0e535d913
Kuro <nelson_minero@yahoo.com>
2021-10-09 11:32:16 -0600

    Añadido typing

--- cogs/anime.py
+++ cogs/anime.py
@@ -13,21 +13,22 @@ class anime(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, co
 
     @commands.command(aliases=['anisearch', 'animesearch'])
     async def anime(self, ctx, *, name):
-      try:
-        anime = animec.Anime(name)
-      except:
-        await ctx.send(embed=discord.Embed(description = "<:notlikethis:868575058283597904> No encontré el anime que estás buscando.", color=discord.Color.red()))
-        return
-      embed = discord.Embed(title=f"{anime.title_jp}\n{anime.title_english}", url=anime.url, description=f"{anime.description[:300]}...", color=discord.Color.random())
-      embed.add_field(name="#️⃣ Episodios:", value=(anime.episodes))
-      embed.add_field(name="👉 Clasificación:", value=str(anime.rating))
-      #embed.add_field(name="📊 Posición:", value=str(anime.ranked))
-      embed.add_field(name="🔎 Estado:", value=str(anime.status))
-      embed.add_field(name="🏷️ Géneros:", value=' '.join((anime.genres)))
-      embed.add_field(name="📺 Tipo:", value=str(anime.type))
+      async with ctx.typing():
+        try:
+          anime = animec.Anime(name)
+        except:
+          await ctx.send(embed=discord.Embed(description = "<:notlikethis:868575058283597904> No encontré el anime que estás buscando.", color=discord.Color.red()))
+          return
+        embed = discord.Embed(title=f"{anime.title_jp}\n{anime.title_english}", url=anime.url, description=f"{anime.description[:300]}...", color=discord.Color.random())
+        #embed.add_field(name="#️⃣ Episodios:", value=(anime.episodes))
+        embed.add_field(name="👉 Clasificación:", value=str(anime.rating))
+        #embed.add_field(name="📊 Posición:", value=str(anime.ranked))
+        embed.add_field(name="🔎 Estado:", value=str(anime.status))
+        embed.add_field(name="🏷️ Géneros:", value=', '.join((anime.genres)))
+        embed.add_field(name="📺 Tipo:", value=str(anime.type))
 
-      embed.set_thumbnail(url=anime.poster)
-      await ctx.send(embed=embed)
+        embed.set_thumbnail(url=anime.poster)
+        await ctx.send(embed=embed)
 
 def setup(bot: commands.Bot):
     bot.add_cog(anime(bot))
