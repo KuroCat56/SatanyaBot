3d6db063413727f1a3a181433a029d5faefe6ba0
Kuro <nelson_minero@yahoo.com>
2021-10-09 13:04:43 -0600

    v0.6.4 - Comandos de anime

--- cogs/general.py
+++ cogs/general.py
@@ -144,7 +144,7 @@ class general(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, 
         
         embed.add_field(
         name="Noticias:",
-        value=f"{block}fix\nPrueba los comandos de traducir, roll y 8ball ヽ(*・ω・)ﾉ\n{block}",
+        value=f"{block}fix\nAhora tengo comandos para buscar anime ヽ(*・ω・)ﾉ\n{block}",
         inline=False
         )
         embed.add_field(
